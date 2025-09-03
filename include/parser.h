e283681572086ee48cb1369cb5224354a88d5899
ztoufik <toufikouedtlelat@gmail.com>
2025-09-03 14:04:57 +0100

    test setup for parser

--- include/parser.h
+++ include/parser.h
@@ -1,7 +1,27 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-int add(int a, int b){return a+b;}
+#include<vector>
+
+#include"lexer.h"
+#include"ast.h"
+ 
+
+template<typename T>class Parser{
+    public:
+        Parser(Lexer<T>&& lexer):lexer(std::move(lexer)){}
+         Program parse(){
+            auto stmt=std::vector<std::shared_ptr<Stmt>>{};
+            stmt.emplace_back(std::make_unique<Double>(3.3));
+            auto program=Program(std::move(stmt));
+            return program;
+        }
+
+    private:
+        Lexer<T> lexer;
+};
+
+
 
 
 #endif 
--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -1,10 +1,17 @@
+#include<memory>
 #include <gtest/gtest.h>
 
+#include "lexer.h"
 #include "parser.h"
 
 TEST(Parser, simpleTest) {
-        int x=2;
-        int y=2;
-        EXPECT_EQ(add(x,y),x+y);
+    auto expected_ast_node=std::make_unique<Double>(3.3);
+    auto parser=Parser(Lexer<std::string_view>("3.3"));
+    Program program=parser.parse();
+    auto& stmt=program.stmts;
+    ASSERT_EQ(stmt.size(),1);
+    auto ast_node=std::dynamic_pointer_cast<Double>(stmt.at(0));
+    ASSERT_TRUE(ast_node);
+    ASSERT_EQ(ast_node->value,expected_ast_node->value);
 }
 
