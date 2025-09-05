7e2de818eadcf1fef628e57d8dbbcce16130e39d
ztoufik <toufikouedtlelat@gmail.com>
2025-09-05 20:20:53 +0100

    run tests if build succeed

--- build.ps1
+++ build.ps1
@@ -1,8 +1,6 @@
 $Arg=$args[0]
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER="clang.exe" -DCMAKE_CXX_COMPILER="clang++.exe" -G Ninja -S . -B build;
-cd build && ninja
-if ($Arg){
- ctest --output-on-failure -R $Arg
-}
+cd build 
+if ((ninja) -and $Arg) { ctest --output-on-failure -R $Arg }
 cd ..
