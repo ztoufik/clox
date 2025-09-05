ddfb3795512f86b68cf6feaaf7df9cee325c65cc
ztoufik <toufikouedtlelat@gmail.com>
2025-09-05 19:38:42 +0100

    run build, LexerTest, ParserTest separately

--- build.ps1
+++ build.ps1
@@ -1,4 +1,8 @@
+$Arg=$args[0]
+
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER="clang.exe" -DCMAKE_CXX_COMPILER="clang++.exe" -G Ninja -S . -B build;
-cd build && ninja && ctest --output-on-failure;
-#cd build && ninja && ctest 
+cd build && ninja
+if ($Arg){
+ ctest --output-on-failure -R $Arg
+}
 cd ..
