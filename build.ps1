ffebe28cb9ebe5e8bad9e50cf13e33670772a52e
ztoufik <toufikouedtlelat@gmail.com>
2025-12-20 15:14:09 +0100

    build main.cpp file for lexing/parsing source.txt file

--- build.ps1
+++ build.ps1
@@ -2,5 +2,10 @@ $Arg=$args[0]
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER="clang.exe" -DCMAKE_CXX_COMPILER="clang++.exe" -G Ninja -S . -B build;
 cd build && ninja 
+switch ($Arg) {
+    "main" {.\test\main.exe }
+    "LexerText" { ctest --output-on-failure -R $Arg }
+    "ParserText" { ctest --output-on-failure -R $Arg  }
+}
 if ($? -and $Arg) { ctest --output-on-failure -R $Arg }
 cd ..
