1cd2f98fdb6df5ff21d2beb93c8054e27a8c88a8
ztoufik <toufikouedtlelat@gmail.com>
2025-12-23 16:35:32 +0100

    build one target at time

--- build.ps1
+++ build.ps1
@@ -1,12 +1,11 @@
 $Arg=$args[0]
 
 #cmake -DLLVM_DIR="D:/LLVM/lib/cmake/llvm" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER="clang.exe" -DCMAKE_CXX_COMPILER="clang++.exe" -G Ninja -S . -B build;
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER="clang.exe" -DCMAKE_CXX_COMPILER="clang++.exe" -G Ninja -S . -B build;
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER="clang.exe" -DCMAKE_CXX_COMPILER="clang++.exe" -G Ninja -S . -B build -DTARGET_TO_BUILD="$Arg";
 cd build && ninja 
 switch ($Arg) {
     "main" {.\test\main.exe }
-    "LexerText" { ctest --output-on-failure -R $Arg }
-    "ParserText" { ctest --output-on-failure -R $Arg  }
+    "Lexer" { ctest --output-on-failure -R "LexerTest"}
+    "Parser" { ctest --output-on-failure -R "ParserTest"}
 }
-if ($? -and $Arg) { ctest --output-on-failure -R $Arg }
 cd ..
