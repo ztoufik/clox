9301d52712bd4ca06498854b35530df7281ef2df
ztoufik <toufikouedtlelat@gmail.com>
2025-12-23 16:02:43 +0100

    LLVM compilation setup in cmake

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 $Arg=$args[0]
 
+#cmake -DLLVM_DIR="D:/LLVM/lib/cmake/llvm" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER="clang.exe" -DCMAKE_CXX_COMPILER="clang++.exe" -G Ninja -S . -B build;
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_C_COMPILER="clang.exe" -DCMAKE_CXX_COMPILER="clang++.exe" -G Ninja -S . -B build;
 cd build && ninja 
 switch ($Arg) {
